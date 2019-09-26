1a22c3aac590c06d92634113e57c5a5b1317cf7a
padrisimo <adrianfernandezchamizo@gmail.com>
2019-09-26 13:53:06 +0200

    focus playground useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Playground from "./battery/Playground.func.js";
-import TodoList from "./todo/TodoList.func.js";
+import Playground from "./focus/Playground.func.js";
+//import Playground from "./battery/Playground.func.js";
+//import TodoList from "./todo/TodoList.func.js";
 
 
-export default () => <TodoList />;
+export default () => <Playground />;
